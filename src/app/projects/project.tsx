e7d9ed374d95fc4eea6f16df42328c8d4ab8af25
Pierrot <faberbastian@gmail.com>
2023-11-09 12:15:07 +0000

    fix project page routing

--- src/app/projects/project.tsx
+++ src/app/projects/project.tsx
@@ -1,13 +1,15 @@
-import ProjectNotFound from "@/common/components/misc/ProjectNotFound";
-import ProjectSection from "@/common/components/section/ProjectSection";
-import { projectsData } from "../../common/resources/data/projects-data";
-import { useParams } from "react-router-dom";
+import ProjectNotFound from '@/common/components/misc/ProjectNotFound'
+import ProjectSection from '@/common/components/section/ProjectSection'
+import { projectsData } from '../../common/resources/data/projects-data'
+import { useParams } from 'react-router-dom'
 
 export default function ProjectPage() {
-  const { projectKey } = useParams() as any;
-  return projectsData[projectKey] ? (
-    <ProjectSection projectData={projectsData[projectKey]}></ProjectSection>
-  ) : (
-    <ProjectNotFound></ProjectNotFound>
-  );
+    const { projectname } = useParams() as any
+    return projectsData[projectname] ? (
+        <ProjectSection
+            projectData={projectsData[projectname]}
+        ></ProjectSection>
+    ) : (
+        <ProjectNotFound></ProjectNotFound>
+    )
 }
--- src/common/components/section/ProjectSection.tsx
+++ src/common/components/section/ProjectSection.tsx
@@ -1,102 +1,119 @@
-import Section from "../misc/Section";
-import StandardButton from "../buttons/StandardButton";
-import RightArrow from "../img/RightArrow";
-import Title from "../text/Title";
-import CustomIcon from "../img/CustomIcon";
-import SubSection from "../misc/SubSection";
-import SubSubTitle from "../text/SubSubTitle";
-import StackTag from "../text/StackTag";
-import CustomLink from "../text/CustomLink";
-import AvatarTag from "../text/AvatarTag";
-import ProjectTag from "../text/ProjectTag";
-import Divider from "../text/Divider";
-import { ProjectData } from "@/common/resources/interfaces/project-data";
+import Section from '../misc/Section'
+import StandardButton from '../buttons/StandardButton'
+import RightArrow from '../img/RightArrow'
+import Title from '../text/Title'
+import CustomIcon from '../img/CustomIcon'
+import SubSection from '../misc/SubSection'
+import SubSubTitle from '../text/SubSubTitle'
+import StackTag from '../text/StackTag'
+import CustomLink from '../text/CustomLink'
+import AvatarTag from '../text/AvatarTag'
+import ProjectTag from '../text/ProjectTag'
+import Divider from '../text/Divider'
+import { ProjectData } from '@/common/resources/interfaces/project-data'
 
 export default function ProjectSection({
-  projectData,
+    projectData,
 }: {
-  projectData: ProjectData;
+    projectData: ProjectData
 }) {
-  const formatter = new Intl.DateTimeFormat("fr", {
-    month: "short",
-    year: "numeric",
-  });
+    const formatter = new Intl.DateTimeFormat('fr', {
+        month: 'short',
+        year: 'numeric',
+    })
 
-  return (
-    <div className="flex flex-col-reverse md:flex-row py-8">
-      <Section className="md:px-10 md:pt-10">
-        <CustomLink href={"/projects"} noblank>
-          <StandardButton
-            prependChild={<RightArrow className="rotate-180"></RightArrow>}
-          >
-            Back to projects
-          </StandardButton>
-        </CustomLink>
-        <Title className="my-3 mt-7">
-          {projectData.title}{" "}
-          <span className="text-base font-normal text-gray-600">
-            {formatter.format(projectData.date)}
-          </span>
-        </Title>
-        <Divider className="my-5"></Divider>
-        <CustomIcon
-          src={projectData.img}
-          className="pt-[75%] my-5 rounded drop-shadow"
-        ></CustomIcon>
-        <p>{projectData.desc}</p>
-      </Section>
-      <div className="md:w-1/2 h-full md:ml-5">
-        <Section>
-          <SubSection>
-            <SubSubTitle>Project Tag</SubSubTitle>
-            <div className="flex flex-row flex-wrap w-full mt-1">
-              {projectData.tags.map((t, i) => (
-                <ProjectTag key={i} tag={t} className="mr-2 my-1"></ProjectTag>
-              ))}
+    return (
+        <div className="flex flex-col-reverse md:flex-row py-8">
+            <Section className="md:px-10 md:pt-10">
+                <CustomLink href={'/projects'} noblank>
+                    <StandardButton
+                        prependChild={
+                            <RightArrow className="rotate-180"></RightArrow>
+                        }
+                    >
+                        Back to projects
+                    </StandardButton>
+                </CustomLink>
+                <Title className="my-3 mt-7">
+                    {projectData.title}{' '}
+                    <span className="text-base font-normal text-gray-600">
+                        {formatter.format(projectData.date)}
+                    </span>
+                </Title>
+                <Divider className="my-5"></Divider>
+                <CustomIcon
+                    src={projectData.img}
+                    className="my-5 rounded drop-shadow"
+                ></CustomIcon>
+                <p>{projectData.desc}</p>
+            </Section>
+            <div className="md:w-1/2 h-full md:ml-5">
+                <Section>
+                    <SubSection>
+                        <SubSubTitle>Project Tag</SubSubTitle>
+                        <div className="flex flex-row flex-wrap w-full mt-1">
+                            {projectData.tags.map((t, i) => (
+                                <ProjectTag
+                                    key={i}
+                                    tag={t}
+                                    className="mr-2 my-1"
+                                ></ProjectTag>
+                            ))}
+                        </div>
+                    </SubSection>
+                    <SubSection className="mt-5">
+                        <SubSubTitle>Project Stack</SubSubTitle>
+                        <div className="flex flex-row flex-wrap w-full mt-1">
+                            {projectData.stack.map((s, i) => (
+                                <StackTag key={i} className="mr-2 my-1">
+                                    {s}
+                                </StackTag>
+                            ))}
+                        </div>
+                    </SubSection>
+                    {projectData.client && (
+                        <SubSection className="mt-5">
+                            <SubSubTitle>Client</SubSubTitle>
+                            {projectData.client?.map((c, i) => (
+                                <AvatarTag
+                                    bg={c.bg}
+                                    src={c.img}
+                                    href={c.link}
+                                    key={i}
+                                >
+                                    {c.name}
+                                </AvatarTag>
+                            ))}
+                        </SubSection>
+                    )}
+                    {(projectData.link || projectData.repository) && (
+                        <div>
+                            {projectData.link && (
+                                <SubSection className="mt-5">
+                                    <SubSubTitle>Link</SubSubTitle>
+                                    <CustomLink
+                                        href={projectData.link}
+                                        decoration
+                                    >
+                                        Visit the project
+                                    </CustomLink>
+                                </SubSection>
+                            )}
+                            {projectData.repository && (
+                                <SubSection className="mt-5">
+                                    <SubSubTitle>Repository</SubSubTitle>
+                                    <CustomLink
+                                        href={projectData.repository}
+                                        decoration
+                                    >
+                                        github.com/LeMoineau
+                                    </CustomLink>
+                                </SubSection>
+                            )}
+                        </div>
+                    )}
+                </Section>
             </div>
-          </SubSection>
-          <SubSection className="mt-5">
-            <SubSubTitle>Project Stack</SubSubTitle>
-            <div className="flex flex-row flex-wrap w-full mt-1">
-              {projectData.stack.map((s, i) => (
-                <StackTag key={i} className="mr-2 my-1">
-                  {s}
-                </StackTag>
-              ))}
-            </div>
-          </SubSection>
-          {projectData.client && (
-            <SubSection className="mt-5">
-              <SubSubTitle>Client</SubSubTitle>
-              {projectData.client?.map((c, i) => (
-                <AvatarTag bg={c.bg} src={c.img} href={c.link} key={i}>
-                  {c.name}
-                </AvatarTag>
-              ))}
-            </SubSection>
-          )}
-          {(projectData.link || projectData.repository) && (
-            <div>
-              {projectData.link && (
-                <SubSection className="mt-5">
-                  <SubSubTitle>Link</SubSubTitle>
-                  <CustomLink href={projectData.link} decoration>
-                    Visit the project
-                  </CustomLink>
-                </SubSection>
-              )}
-              {projectData.repository && (
-                <SubSection className="mt-5">
-                  <SubSubTitle>Repository</SubSubTitle>
-                  <CustomLink href={projectData.repository} decoration>
-                    github.com/LeMoineau
-                  </CustomLink>
-                </SubSection>
-              )}
-            </div>
-          )}
-        </Section>
-      </div>
-    </div>
-  );
+        </div>
+    )
 }
