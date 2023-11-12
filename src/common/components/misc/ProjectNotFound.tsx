16e817cc6416eaa9f6e82af7cebeaea3df724266
Pierrot <faberbastian@gmail.com>
2023-11-12 12:48:02 +0000

    project not found trad

--- src/common/components/misc/ProjectNotFound.tsx
+++ src/common/components/misc/ProjectNotFound.tsx
@@ -1,15 +1,23 @@
+import { useTranslation } from 'react-i18next'
 import MyProjectsSubSection from '../section/MyProjectsSubSection'
 import CustomLink from '../text/CustomLink'
 import Title from '../text/Title'
 import Section from './Section'
 
 export default function ProjectNotFound() {
+    const { t } = useTranslation()
     return (
         <div>
             <div className="text-center">
                 <Title className="mt-24 mb-5">
-                    <span className="text-red-500">Crap</span>! The project you
-                    find not exist <span className="text-purple-400">yet</span>!
+                    <span className="text-red-500">
+                        {t('project-not-found.title1')}
+                    </span>
+                    {t('project-not-found.title2')}{' '}
+                    <span className="text-purple-400">
+                        {t('project-not-found.title3')}
+                    </span>
+                    !
                 </Title>
                 <p>
                     It seems that the project you are looking for does not yet
